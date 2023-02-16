3e1012b9b0c0c6be1fff221f80ffced86dc17909
ilvostrofighissimousername <lavostramail@gmail.com>
2023-02-16 14:36:40 +0100

    prog_2

---
+++ bin/prog_2.dart
@@ -0,0 +1,5 @@
+import 'package:prog_2/prog_2.dart' as prog_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${prog_2.calculate()}!');
+}
---
+++ test/prog_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:prog_2/prog_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
